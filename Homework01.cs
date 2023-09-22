ef1abeb40c8e8fc508701d21f44025ccb347588d
iryna.abramchenkova <iryna.abramchenkova@KBP1-LHP-A10426>
2023-09-22 13:03:33 +0300

    Add project files.

---
+++ Homework01.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+//var name = "";
+//Console.WriteLine("What is your name?");
+//name = Console.ReadLine();
+//Console.WriteLine($"Hello,{name}!");
+//Console.WriteLine("Nice to meet you!");
+//Console.ReadLine();
+
+string Meet()
+{
+    Console.WriteLine("What's your name?");
+    return Console.ReadLine();
+}
+
+void Greet(string name)
+{
+    Console.WriteLine($"Hello, {name}!");
+}
+var name = Meet();
+Greet(name);
+
+
+//Console.WriteLine("What's your name?");
+//var name = Console.ReadLine();
+//Console.WriteLine($"Hello, {name}!");
+
+//Console.WriteLine($"Hello, has {"Hello,".Length} letters");
